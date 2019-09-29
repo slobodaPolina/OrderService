ff28360bb3612338116a79e29c07b49d76152649
Daniil Sloboda <dsloboda@cloudlinux.com>
2019-09-29 14:51:23 +0300

    реализовала взятие заказа по id

--- src/main/java/dao/OrderDAO.java
+++ src/main/java/dao/OrderDAO.java
@@ -4,15 +4,14 @@ import dto.OrderDTO;
 import entity.Order;
 import service.SessionFactoryService;
 import org.hibernate.Session;
-
-import javax.persistence.criteria.CriteriaQuery;
-import java.sql.SQLException;
+import javax.persistence.criteria.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class OrderDAO {
-	public List<OrderDTO> getOrders() throws SQLException {
-		SessionFactoryService sfService = new SessionFactoryService();
+	private SessionFactoryService sfService = new SessionFactoryService();
+
+	public List<OrderDTO> getOrders() {
 		Session session = null;
 		List<Order> orders = null;
 		try {
@@ -28,11 +27,29 @@ public class OrderDAO {
 		return orders.stream().map(order -> new OrderDTO(order)).collect(Collectors.toList());
 	}
 
+	public OrderDTO getOrderById(long orderId) {
+		Session session = null;
+		Order order = null;
+		try {
+			session = sfService.getOpenedSession();
+			CriteriaQuery<Order> query = session.getCriteriaBuilder().createQuery(Order.class);
+			Root<Order> root = query.from(Order.class);
+			query.select(root).where(root.get("id").in(orderId));
+			order = getSingleResult(session, query);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sfService.closeSession(session);
+		}
+		return order == null ? null : new OrderDTO(order);
+	}
+
 	public OrderDTO createOrder() {
 		return null;
 	}
 
-	public OrderDTO getOrderById(long orderId) {
-		return null;
+	private <T> T getSingleResult(Session session, CriteriaQuery<T> query) {
+		List<T> results = session.createQuery(query).getResultList();
+		return !results.isEmpty() ? results.get(0) : null;
 	}
 }
