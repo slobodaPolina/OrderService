e062dad4f2fe0df57cc6e5157a17696f3add8e12
Daniil Sloboda <sdsdaniil@gmail.com>
2019-11-25 14:10:54 +0300

    returning JSON

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,40 +1,64 @@
 import static spark.Spark.*;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import dao.*;
 import dto.*;
 import entity.Status;
 import service.*;
 
+import java.util.stream.Collectors;
+
 public class Main {
 	private static OrderService orderService = new OrderService(new OrderDAO(), new CommonDAO());
+
+	private static GsonBuilder builder = new GsonBuilder();
+	private static Gson gson = builder.create();
 
     public static void main(String[] args) {
 		MessagingService.setupListener(orderService, new CommonDAO(), new OrderDAO());
 
     	port(1809);
 
-		get("/api/orders", (req, res) -> orderService.getOrders());
+		get(
+			"/api/orders",
+			(req, res) ->
+				orderService.getOrders().stream()
+					.map(orderDTO -> gson.toJson(orderDTO))
+					.collect(Collectors.toList())
+		);
 
 		post("/api/orders/:username", (req, res) ->
-				new OrderDTO(orderService.createEmptyOrder(req.params("username")))
+			gson.toJson(
+				new OrderDTO(
+					orderService.createEmptyOrder(req.params("username"))
+				)
+			)
 		);
 
 		get("/api/orders/:orderId", (req, res) ->
-			orderService.getOrderDTOById(Long.parseLong(req.params("orderId")))
+			gson.toJson(
+				orderService.getOrderDTOById(
+					Long.parseLong(req.params("orderId"))
+				)
+			)
 		);
 
 		put("/api/orders/:orderId/status/:status", (req, res) ->
-			orderService.changeOrderStatus(
-				Long.parseLong(req.params("orderId")),
-				Status.valueOf(req.params("status"))
+			gson.toJson(
+				orderService.changeOrderStatus(
+					Long.parseLong(req.params("orderId")),
+					Status.valueOf(req.params("status"))
+				)
 			)
 		);
 
 		post("/api/orders/:orderId/item", (req, res) ->
-			orderService.addItemToOrder(
-				parseLong(req.params("orderId")),
-				new Gson().fromJson(req.body(), ItemAdditionParametersDTO.class)
+			gson.toJson(
+				orderService.addItemToOrder(
+					parseLong(req.params("orderId")),
+					new Gson().fromJson(req.body(), ItemAdditionParametersDTO.class)
+				)
 			)
 		);
     }
